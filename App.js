bffafded18510a55f7094977de3d2085bc68b8b5
Estéfanas ♡ <estefanasargentina3@gmail.com>
2026-02-05 07:49:08 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Estéfanas Argentina Castanheira de Mesquita</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff9fbf',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
